3d07f8f25e9a30ecf71d1ddfb9abe8934cd8653b
Mohammed Umer <52200381+mohdumer7@users.noreply.github.com>
2023-04-17 15:07:01 +0530

    Added relative path

--- src/pages/Dummy.js
+++ src/pages/Dummy.js
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
+
 export default function Dummy() {
   return (
     <>
       <h1>Dummy</h1>
+      {/* This is a LINK element acts as an anchor element
+          this helps you navigate around the relative defines the
+          the type of navigation to take place if absolte routing then
+          choose relative="path" else use relative="route" for relative routing
+      */}
+      <Link to="products" relative="route">
+        products
+      </Link>
+      {/* The relative case can also be seen here where 
+          just by writing LINK=".." will go remove all chilren routing
+          adn become absolute while adding relative goes back using children
+          routing too
+      */}
+      <Link to=".." relative="route">
+        back one
+      </Link>
+      <Link to=".." relative="path">
+        back home to main page
+      </Link>
     </>
   );
 }
